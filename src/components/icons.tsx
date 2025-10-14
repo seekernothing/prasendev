cfb373a1bc1c1ace53d115747a977dc18d731073
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-10-14 22:40:50 +0530

    refactor: streamline SVG component formatting and consolidate files icon definition

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -189,7 +189,7 @@ export const Icons = {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
-      height="24" 
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -293,7 +293,5 @@ export const Icons = {
       />
     </svg>
   ),
-  files: (props: IconProps) => (
-    <FolderIcon {...props} />
-  ),
+  files: (props: IconProps) => <FolderIcon {...props} />,
 };
